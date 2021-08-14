afced7bc8dacdf0d3b99cbafe8b84fa44ee9516a
Zapster <findingslimes@gmail.com>
2021-08-13 23:21:02 -0400

    Create questionprompts.js

---
+++ questionprompts.js
@@ -0,0 +1,18 @@
+//PromptList
+
+var prompts = [
+   "When people have dreams about flying, it means ____.///When people have that dream about being naked in school, it means ____.",
+   "When people have that dream about being naked in school, it means ____.///When people have dreams about flying, it means ____.",
+   "What's that worst thing to say on a first date?///What's the last text you sent?",
+   "What's the last text you sent?///What's that worst thing to say on a first date?",
+   "What does your Twitter/Instagram bio say?///What does/would your tinder bio say?",
+   "What does/would your tinder bio say?///What does your Twitter/Instagram bio say?",
+   "God just added a new commandment. It reads ____.///What is the unwritten law of the internet?",
+   "What is the unwritten law of the internet?///God just added a new commandment. It reads ____.",
+   "What is your favorite food?///What was the last thing you ate?",
+   "What was the last thing you ate?///What is your favorite food?",
+   "What was the last thing you ate?///What is your least favorite food?",
+   "What is your least favorite food?///What was the last thing you ate?",
+   "Who is your least favorite celebrity?///Who do you love?",
+   "Who do you love?///Who is your least favorite celebrity?"
+ ]
